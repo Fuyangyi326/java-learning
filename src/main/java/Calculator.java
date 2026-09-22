c9a5e28362cf006515c57543e4ec44e301b005d2
Fuyangyi <GitHub>
2026-09-22 11:17:33 +0800

    练习：算术运算符

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class Calculator {
+    static void main(String[] args) {
+        int a = 15;
+        int b = 4;
+        System.out.println("a+b="+(a+b));
+        System.out.println("a-b="+(a-b));
+        System.out.println("a*b="+(a * b));
+        System.out.println("a/b="+(a/b));
+        System.out.println("a%b="+(a%b));
+    }
 }
+
